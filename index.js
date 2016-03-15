dc83b6ad2fdb1e04644eff97b5b8854efc82a40e
Jim Lin <jim3232ya@hotmail.com>
2016-03-15 12:30:05 +0800

    Leaflet map Quick Start

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+
+app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.render('index');
 });
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');

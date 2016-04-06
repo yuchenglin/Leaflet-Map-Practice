51be31d535a7d2625066423360331aa2a4c1fa73
Jim Lin <jim3232ya@hotmail.com>
2016-04-06 22:40:04 +0800

    Add socket.io

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
+// var express = require('express');
+// var app = express();
+// var path = require('path');
+
+// var	server = require('http').Server(app);
+// // var io = require('socket.io')(server);
+
+
+// app.use(express.static(path.join(__dirname,'public')));
+
+// app.get('/', function (req, res) {
+//   res.sendfile(__dirname + '/index.html');
+// });
+
+// app.listen(3000, function () {
+//   console.log('Example app listening on port 3000!');
+// });
+
+// io.listen(app);
+
 var express = require('express');
 var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 var path = require('path');
+
+server.listen(3000);
 
 app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/', function (req, res) {
-  res.render('index');
+  res.sendfile(__dirname + '/index.html');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
 });

f560acd677848bb0141cb654f93db88003bacc48
Jim Lin <jim3232ya@hotmail.com>
2016-04-07 10:36:02 +0800

    next : socket.io + map

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
-// var express = require('express');
-// var app = express();
-// var path = require('path');
-
-// var	server = require('http').Server(app);
-// // var io = require('socket.io')(server);
-
-
-// app.use(express.static(path.join(__dirname,'public')));
-
-// app.get('/', function (req, res) {
-//   res.sendfile(__dirname + '/index.html');
-// });
-
-// app.listen(3000, function () {
-//   console.log('Example app listening on port 3000!');
-// });
-
-// io.listen(app);
-
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
@@ -25,6 +5,9 @@ var io = require('socket.io')(server);
 var path = require('path');
 
 server.listen(3000);
+// app.listen(3000, function () {
+//   console.log('Example app listening on port 3000!');
+// });
 
 app.use(express.static(path.join(__dirname,'public')));
 
@@ -33,7 +16,7 @@ app.get('/', function (req, res) {
 });
 
 io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
+  socket.emit('news', { hello: 'Gee Gee Gee Gee baby baby baby' });
   socket.on('my other event', function (data) {
     console.log(data);
   });
